611149dbf8f07c0b6fadc31f02f879bc2d88ed2c
gfreitas <gustavo.freitas.afel@gmail.com>
2022-10-20 14:23:48 -0300

    "Corrigindo o desafio INTER"

--- controledefluxo/src/desafio/inter/MenorPosicao.java
+++ controledefluxo/src/desafio/inter/MenorPosicao.java
@@ -1,16 +1,31 @@
 package inter;
 
 import java.util.Scanner;
+import java.io.IOException;
 
 
 public class MenorPosicao {
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         Scanner entrada = new Scanner(System.in);
         int E = entrada.nextInt();
+        int[] x = new int[E];
+        int numero = 0;
+        int pos = 0;
        
             for(int i = 0; i < E; i++){
+                x[i] = entrada.nextInt();
+                    if(i == 0){
+                        numero = i;
+                        pos = i;
+                    }else if(x[i] < numero){
+                        numero = x[i];
+                        pos = i;
+                    }
             }
+
+        System.out.println("Menor valor: " + numero);
+        System.out.println("Posição: " + pos);
           
 
         entrada.close();
