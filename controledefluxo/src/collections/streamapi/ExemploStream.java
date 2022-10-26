cfecba5c01421a258c4b152288f903f64d345665
gfreitas <gustavo.freitas.afel@gmail.com>
2022-10-26 11:55:11 -0300

    "Stream() final"

--- controledefluxo/src/collections/streamapi/ExemploStream.java
+++ controledefluxo/src/collections/streamapi/ExemploStream.java
@@ -2,10 +2,9 @@ package collections.streamapi;
 
 import java.util.ArrayList;
 
-import java.util.ArrayList;
+
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ExemploStream {
@@ -34,9 +33,45 @@ public class ExemploStream {
         System.out.println("Menor número de letras: " + estudantes.stream().min(Comparator.comparingInt(String::length)));
 
         // Retorna os elementos que tem a letra R no nome
-        System.out.println("Com a letra r no nome: " + estudantes.stream().filter((estudantes) -> estudantes.toLowerCase().contains("r")).collect(Collectors.toList()));
+        System.out.println("Com a letra r no nome: " + estudantes.stream().filter((estudante) -> estudante.toLowerCase().contains("r")).collect(Collectors.toList()));
 
         // Retorna uma nova coleção, com os nomes concatenados a quantidade de letras de cada nome
-        System.out.println("Retorna uma nova coleção com a quantidade de letras: " + estudantes.stream().map(estudantes));
+        System.out.println("Retorna uma nova coleção com a quantidade de letras: " + estudantes.stream().map(estudante -> estudante.concat(" -- ").concat(String.valueOf(estudante.length()))).collect(Collectors.toList()));
+
+        // Retorna somente os 3 primeiros elementos da coleção
+        System.out.println("Retorna os 3 primeiros elementos: " + estudantes.stream().limit(3).collect(Collectors.toList()));
+
+        // Exibe cada elemento no console, e depois retorna a mesma coleção
+        System.out.println("Retorna os elementos: " + estudantes.stream().peek(System.out::println).collect(Collectors.toList()));
+
+        // Exibe cada elemento no console sem retornar outra coleção
+        System.out.println("retorna os elementos novamente: ");
+        estudantes.stream().forEach(System.out::println);
+
+        // Retorna true se todos os elementos possuem a letra W no nome
+        System.out.println("Tem algum elemento com W no nome " + estudantes.stream().allMatch((elemento) -> elemento.contains("W")));
+
+        // Retorna true se algum dos elementos possuem a letra a minúscula no nome
+        System.out.println("Tem algum elemento com a minúscula no nome? " + estudantes.stream().anyMatch((elemento) -> elemento.contains("a")));
+
+        // Retorna true se nenhum elemento possue a letra minúscula no nome
+        System.out.println("Nao tem nenhum elemento com a minúscula no nome? " + estudantes.stream().noneMatch((elemento) -> elemento.contains("a")));
+
+        // Retorna o primeiro elemento da coleção, se existir exibe no console
+        System.out.println("Retorna o primeiro elemento da coleção: ");
+        estudantes.stream().findFirst().ifPresent(System.out::println);
+
+        // Exemplo de operação encadeada
+        System.out.println("Operação encadeada: ");
+        System.out.println(estudantes.stream()
+        .peek(System.out::println)
+        .map(estudante ->
+            estudante.concat(" -- ").concat(String.valueOf(estudante.length())))
+            .peek(System.out::println)
+            .filter((estudante) -> estudante.toLowerCase().contains("r"))
+//            .collect(Collectors.toList()));
+//            .collect(Collectors.joining(", ")));
+//            .collect(Collectors.toSet()));
+            .collect(Collectors.groupingBy(estudante -> estudante.substring(estudante.indexOf(" -- ") + 1))));
     }
 }
