d50c58de921ec2091d69f7382bc1c82552e692f0
gfreitas <gustavo.freitas.afel@gmail.com>
2022-10-26 12:47:32 -0300

    "Paradigma Funcional"

---
+++ controledefluxo/src/paradigmasjava/Aula1.java
@@ -0,0 +1,10 @@
+package paradigmasjava;
+
+public class Aula1 {
+    
+    public static void main(String[] args) {
+        int valor = 10;
+        int resultado = valor * 3;
+        System.out.println("O resultado é: " + resultado);
+    }
+}
